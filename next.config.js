9f75762046679c6133fe69a64688b95b18cd6871
Matej Husar <matejhusar@gmail.com>
2023-01-13 23:39:36 +0100

    Implementing next auth provider against AWS Cognito

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  // Call federated logout (logout on the auth provider side)
+  async redirects() {
+    return [
+      {
+        source: '/logout',
+        destination: `${process.env.COGNITO_LOGOUT_URL}?client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.NEXTAUTH_URL}`,
+        permanent: true
+      }
+    ];
+  }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </ChakraProvider>
   );
 }
