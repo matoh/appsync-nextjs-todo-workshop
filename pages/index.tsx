ff1c2715e6ea8194f0592afc07e0a5dc5fedf1a8
Matej Husar <matejhusar@gmail.com>
2023-01-15 12:43:02 +0100

    Simple rewrite switching from state to reducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
 import { Text, Card, CardBody, HStack, Stack, Heading, Button, Input, FormControl, Flex } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import useColorModeValue from '../hooks/useColorModeValue';
 import { v4 as uuid } from 'uuid';
+
+enum ActionType {
+  ADD_ITEM = 'addItem',
+  DELETE_ITEM = 'deleteItem'
+}
+
+interface ActionAddItem {
+  type: ActionType.ADD_ITEM;
+  todo: TodoItem;
+}
+
+interface ActionDeleteItem {
+  type: ActionType.DELETE_ITEM;
+  id: string;
+}
+
+type ActionTypes = ActionAddItem | ActionDeleteItem;
+
+function reducer(todoItems: TodoItem[], action: ActionTypes): TodoItem[] {
+  switch (action.type) {
+    case ActionType.ADD_ITEM:
+      return [...todoItems, action.todo];
+    case ActionType.DELETE_ITEM:
+      return todoItems.filter((todoItem) => todoItem.id !== action.id);
+  }
+}
 
 export interface TodoItem {
   id: string;
@@ -10,16 +36,13 @@ export interface TodoItem {
 }
 
 export default function Home() {
-  const [todoItems, setTodoItems] = useState<TodoItem[]>([
-    { id: uuid(), text: 'Task 1' },
-    { id: uuid(), text: 'Task 2' }
-  ]);
+  const [todoItems, dispatch] = useReducer(reducer, []);
   const [newItem, setNewItem] = useState('');
   const { color } = useColorModeValue();
 
   const addNewItem = () => {
     if (newItem) {
-      setTodoItems([...todoItems, { id: uuid(), text: newItem }]);
+      dispatch({ type: ActionType.ADD_ITEM, todo: { id: uuid(), text: newItem } });
       setNewItem('');
     }
   };
@@ -40,7 +63,7 @@ export default function Home() {
           <CardBody p='3'>
             <Flex alignItems='center' justifyContent='space-between'>
               <Text color={color('black', 'white')}>{item.text}</Text>
-              <Button onClick={() => setTodoItems(todoItems.filter((todoItem) => todoItem.id !== item.id))}>
+              <Button onClick={() => dispatch({ type: ActionType.DELETE_ITEM, id: item.id })}>
                 <FaTrash />
               </Button>
             </Flex>
