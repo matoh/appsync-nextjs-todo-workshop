0d9b545064cbf7ab6388a906ef5480a8ce428509
Matej Husar <matejhusar@gmail.com>
2023-01-31 22:34:45 +0100

    Layout improvement when user is authenticated/unauthenticated

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,14 +1,16 @@
-import { ReactElement } from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { ReactElement } from 'react';
+import { Box, Heading, Highlight } from '@chakra-ui/react';
 import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import useColorModeValue from '../../hooks/useColorModeValue';
 import HeaderBar from './HeaderBar';
+import { useSession } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Layout({ children }: { children: ReactElement }) {
   const { color } = useColorModeValue();
+  const { status } = useSession();
 
   return (
     <>
@@ -21,8 +23,15 @@ export default function Layout({ children }: { children: ReactElement }) {
       <main className={inter.className}>
         <Box maxWidth='68rem' margin='auto' height='100vh' backgroundColor={color('gray.50', 'gray.600')}>
           <HeaderBar />
-          <Box p='4' >
-            {children}
+          <Box p='4'>
+            {status === 'authenticated' && children}
+            {status === 'unauthenticated' && (
+              <Heading size='xl' m='10' textAlign='center'>
+                <Highlight query='Sign in' styles={{ px: '2', py: '2', borderRadius: '10', bg: 'gray.200' }}>
+                  Sign in to manage todo items
+                </Highlight>
+              </Heading>
+            )}
           </Box>
         </Box>
       </main>
